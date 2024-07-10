c772471dadbc072f8df5af219b363e36f227f749
Sungjin Han <meinside@meinside.dev>
2024-07-10 14:09:02 +0900

    feat(nvim): add :GeminiCommitLog command
    This commit introduces a new command, `:GeminiCommitLog`, that generates nicely formatted commit logs from either the current buffer or a selected text range. This will make it easier for users to view and understand commit history within the editor.

--- README.md
+++ README.md
@@ -41,7 +41,9 @@ and create a JSON config file at path `configFilepath` with following content:
 
 ## Usage
 
-### Insert Generated Text At Current Cursor Position
+### Text Generation
+
+#### Insert Generated Text At Current Cursor Position
 
 ![gemini-nvim insert-with-prompt](https://github.com/meinside/gemini.nvim/assets/185988/f0575fe1-b40d-4962-9cec-f22818635767)
 
@@ -53,7 +55,7 @@ Run following command with a prompt:
 
 It will generate a text from your prompt and insert it at the current cursor position.
 
-### Generate Text With Selected Range As A Prompt
+#### Generate Text With Selected Range As A Prompt
 
 ![gemini-nvim replace](https://github.com/meinside/gemini.nvim/assets/185988/aeb5aee1-0078-4407-9acd-e9628b519420)
 
@@ -65,7 +67,7 @@ Select a range of text with visual block, and run following command:
 
 then it will generate a text from the selected text as a prompt, and replace the range with the generated one.
 
-### Replace Selected Range With Generated Text
+#### Replace Selected Range With Generated Text
 
 ![gemini-nvim replace-with-prompt](https://github.com/meinside/gemini.nvim/assets/185988/831aa4f2-cfb9-4253-8cf6-e585b7617284)
 
@@ -77,13 +79,32 @@ Select a range of text with visual block, and run following command with a promp
 
 then it will generate a text from both the selected text and prompt, and replace the range with the generated one.
 
+### Commit Message Generation
+
+#### Generate a Commit Message with Current Buffer
+
+Run following command:
+
+```
+:GeminiCommitLog
+```
+
+#### Replace Selected Range With Generated Commit Message
+
+Select a range of text with visual block, and run following command:
+
+```
+:'<,'>GeminiCommitLog
+```
+
 ## Todos / Improvements
 
-- [X] Add screen recordings for each usage.
+- [X] Add screen recordings for text generation.
+- [ ] Add screen recordings for commit log generation.
 - [X] Strip unwanted markdown codeblock around the generated texts.
+- [ ] Add nice UIs for comparing & applying generated texts.
 - [X] Add an option for setting safety threshold.
 - [ ] Handle multiple number of candidates and content parts.
-- [ ] Add nice UIs for comparing & applying generated texts.
 
 ## License
 
--- plugin/gemini.lua
+++ plugin/gemini.lua
@@ -2,7 +2,7 @@
 --
 -- Gemini plugin for neovim
 --
--- last update: 2024.02.28.
+-- last update: 2024.07.10.
 
 local gemini = require'gemini'
 local config = require'gemini/config'
@@ -97,4 +97,85 @@ vim.api.nvim_create_user_command(
   end,
   { range = true, nargs = '?' }
 )
+
+-- :GeminiCommitLog
+--   generate a nice commit log from the content of current file (eg. COMMIT_EDITMSG)
+--
+-- :'<,'>GeminiCommitLog
+--   replace selected range with generated content from the selected range as a prompt
+--
+vim.api.nvim_create_user_command(
+  'GeminiCommitLog',
+  function(opts)
+    debug('opts of command `GeminiCommitLog`: ' .. vim.inspect(opts))
+
+    local prompt = 'Please generate a commit message adhering to the Conventional Commits v1.0.0 specification:\n\n'
+
+    -- generate texts with given prompt,
+    if opts.range == 0 then -- if there was no selected range,
+      local text = util.remove_comments(ui.whole_buffer_lines())
+      local prompts = { prompt .. text }
+
+      debug('using prompt: ' .. prompts[1])
+
+      -- do the generation
+      local parts, err = gemini.generate(prompts)
+      if err ~= nil then
+        error(err)
+      else
+        -- strip outermost codeblock
+        if config.options.stripOutermostCodeblock() then
+          for i, _ in ipairs(parts) do
+            parts[i] = util.strip_outermost_codeblock(parts[i])
+          end
+        end
+
+        local lines = {}
+        for i, _ in ipairs(parts) do
+          local splitted = util.split(parts[i], '\n')
+          for j, _ in  ipairs(splitted) do
+            table.insert(lines, splitted[j])
+          end
+        end
+
+        -- and replace whole file with the generated content
+        ui.replace_whole_text(lines)
+      end
+    else -- if there was some selected range,
+      local start_row, start_col, end_row, end_col = ui.get_selection()
+      local selected = ui.get_text(start_row, start_col, end_row, end_col)
+
+      local prompts = {}
+      if selected ~= nil then
+        debug('using selected range as a prompt: ' .. selected)
+
+        table.insert(prompts, prompt .. selected)
+      end
+      if #opts.fargs > 0 then
+        debug('using command parameter as a prompt: ' .. opts.fargs[1])
+
+        table.insert(prompts, prompt .. opts.fargs[1])
+      end
+
+      -- do the generation
+      local parts, err = gemini.generate(prompts)
+      if err ~= nil then
+        error(err)
+      else
+        -- strip outermost codeblock
+        if config.options.stripOutermostCodeblock() then
+          for i, _ in ipairs(parts) do
+            parts[i] = util.strip_outermost_codeblock(parts[i])
+          end
+        end
+        local generated = util.join(parts, '\n\n')
+
+        -- and replace the selected range with generated content
+        ui.replace_text(start_row, start_col, end_row, end_col, generated)
+      end
+    end
+  end,
+  { range = true }
+)
+
 
